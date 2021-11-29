9484b2176d7d11dcb9a6b328d28d08ba7a679068
Alexander-Witwer2 <89601320+Alexander-Witwer2@users.noreply.github.com>
2021-11-29 00:48:04 -0500

    Add files via upload

--- banker.cpp
+++ banker.cpp
@@ -8,7 +8,7 @@ int main(){
     if(!(input.is_open())) exit(EXIT_FAILURE);
 
     Banker money(input);
-
+    money.print();
     money.bankAlgor();
 
     return 0;
--- banker.hpp
+++ banker.hpp
@@ -9,12 +9,10 @@ class Banker{
     static constexpr int resNum = 3;
     static constexpr int procNum = 5;
     
-    bool flag[procNum];
     int allocated[procNum][resNum];
     int max[procNum][resNum];
     int need[procNum][resNum];
     int available[resNum];
-    int procOrd[procNum];
 
     Banker(){};
     Banker(std::ifstream& feed){
@@ -33,51 +31,96 @@ class Banker{
                     feed >> available[i];
                 }
     }
-    need[procNum][resNum] = subMultArr(allocated,max);
+        calculateNeed(need,max,allocated);
     }
 
-    void bankAlgor(){
+    bool bankAlgor(){
+        bool flag[procNum] = {0};
+        int procOrd[procNum];
+        int count = 0;
+        int work[resNum];
+        for(int i = 0; i < resNum; ++i){
+            work[i] = available[i];
+        }
+
+        bool found;
+        while(count < procNum){
+        found = false;
+            for(int i = 0; i < procNum; i++){
+                if(flag[i] == 0){
+                    int j;
+                    for(j = 0; j < resNum; ++j){
+                        if(need[i][j] > work[j]) break;
+                        }
+                        if(j == resNum){
+                            for (int k = 0 ; k < resNum ; k++)
+                            work[k] += allocated[i][k];
+                            
+                        procOrd[count] = i+1;
+                        count++;
+                        flag[i]= 1;
+
+                        found = true;
+                        }
+                }
+            }
+            if (found == false)
+                    {
+                        std::cout << "System is not in safe state";
+                        return false;
+                    }
+        }
+    std::cout << "System is in safe state.\nSafe"
+        " sequence is: ";
+    for (int i = 0; i < procNum; i++)
+        std::cout << procOrd[i] << " ";
+  
+    return true;
+    }
+
+    void print(){
         for(int i = 0; i < procNum; ++i){
             for(int j = 0; j < resNum; ++j){
-                std::cout << allocated[i][j];
+                std::cout << allocated[i][j] << " ";
             }
             std::cout << std::endl;
         }
+        std::cout << std::endl;
             for(int i = 0; i < procNum; ++i){
                 for(int j = 0; j < resNum; ++j){
-                    std::cout << max[i][j];
+                    std::cout << max[i][j] << " ";
                 }
                 std::cout << std::endl;
             }
+            std::cout << std::endl;
                 for(int i = 0; i < resNum; ++i){
-                    std::cout << available[i];
+                    std::cout << available[i] << " ";
                 }
-                
+                    std::cout << std::endl;
+
+                std::cout << std::endl;
                 for(int i = 0; i < procNum; ++i){
                     for(int j = 0; j < resNum; ++j){
-                        std::cout << need[i][j];
+                        std::cout << need[i][j] << " ";
             }
+            std::cout << std::endl;
         }
-                std::cout << std::endl;
+            std::cout << std::endl;
     }
 
-    int addMultArr (int mat[procNum][resNum],int rix[procNum][resNum]){
-        int output[procNum][resNum];
-        for(int i = 0; i < procNum; ++i){
-            for(int j = 0; j < resNum; ++j){
-                output[i][j]=mat[i][j] - rix[i][j];
-            }
-        }
-        return output[procNum][resNum];
-    }
-    int subMultArr (int mat[procNum][resNum],int rix[procNum][resNum]){
-        int output[procNum][resNum];
+    void addMultArr (int output[procNum][resNum],int mat[procNum][resNum],int rix[procNum][resNum]){
         for(int i = 0; i < procNum; ++i){
             for(int j = 0; j < resNum; ++j){
                 output[i][j]=mat[i][j] + rix[i][j];
             }
         }
-        return output[procNum][resNum];
+    }
+    void calculateNeed (int output[procNum][resNum],int mat[procNum][resNum],int rix[procNum][resNum]){
+        for(int i = 0; i < procNum; ++i){
+            for(int j = 0; j < resNum; ++j){
+                output[i][j]=mat[i][j] - rix[i][j];
+            }
+        }
     }
 };
 
